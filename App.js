983f1cf08fbfec69963467b19d61e68da11236fd
Gustavo <gustavo472017@hotmail.com>
2023-08-21 19:45:18 -0300

    a bc

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import Card from './components/Card';
 
 export default function App() {
   return (
-    <ScrollView style={{marginTop:30}}>
+    <ScrollView style={{marginTop:35}}>
 <Card/>
     </ScrollView>
   );
